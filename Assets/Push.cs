b2d2e6731add78680677ebd61586c2f0af178ac0
Eduardo Antolín Jiménez <laloantolin@hotmail.com>
2019-02-24 21:44:29 -0700

    Tarea fisica
    Bolita que le pega a otra bolita

---
+++ Assets/Push.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.AddForce(Vector2.left * 24, ForceMode2D.Impulse);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
